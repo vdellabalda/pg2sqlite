cabfb7cf55abfa39c7f9604b64adf502cdec5ee0
vdellabalda <vinc.dellabalda@gmail.com>
2025-05-13 11:44:47 +0200

    more adjustments

--- initial-sync/py/import_csv_to_sqlite.py
+++ initial-sync/py/import_csv_to_sqlite.py
@@ -1,42 +1,37 @@
-import os
 import sys
 import subprocess
+from pysqlite3 import dbapi2 as sqlite3
 import pandas as pd
-import sqlite3
+from sqlalchemy import create_engine
+
 
 def import_csv_to_sqlite(csv_directory, sqlite_db_file, schema_sql_file=None):
     # Step 1: Load schema into the SQLite database
+    engine = create_engine(f"sqlite:///{sqlite_db_file}")
+    conn = sqlite3.connect(sqlite_db_file)
+    cur = conn.cursor()
+
     if schema_sql_file:
         print(f"Loading schema from {schema_sql_file} into {sqlite_db_file}...")
-        try:
-            subprocess.run(f'sqlite3 "{sqlite_db_file}" < "{schema_sql_file}"', shell=True, check=True)
-            print("✅ Schema applied successfully.")
-        except subprocess.CalledProcessError as e:
-            print(f"❌ Failed to load schema: {e}")
-            return
+        with open(schema_sql_file) as fp:
+            cur.executescript(fp.read())  # or con.executescript
+
+        #try:
+        #    subprocess.run(f'sqlite3 "{sqlite_db_file}" < "{schema_sql_file}"', shell=True, check=True)
+        #    print("✅ Schema applied successfully.")
+        #except subprocess.CalledProcessError as e:
+        #    print(f"❌ Failed to load schema: {e}")
+        #    return
 
     # Step 2: Import all CSV files into the pre-created tables
     csv_files = [f for f in os.listdir(csv_directory) if f.endswith('.csv')]
     for csv_file in csv_files:
         table_name = os.path.splitext(csv_file)[0]
-        df = pd.read_csv(csv_directory+"/"+csv_file, header=0)  # skips the first row
-        print(df.head())
-        conn = sqlite3.connect(sqlite_db_file)
-        df.to_sql(table_name, conn, if_exists="append", index=False)
-        #csv_file_path = os.path.join(csv_directory, csv_file)
+        csv_file_path = os.path.join(csv_directory, csv_file)
+        df = pd.read_csv(csv_file_path, header=0)  # skips the first row
 
         print(f"📥 Importing {csv_file} into table {table_name}...")
-        #sqlite_command = f'''
-        #sqlite3 "{sqlite_db_file}" <<EOF
-        #.mode csv
-        #.import "{csv_file_path}" "{table_name}"
-        #EOF
-        #'''
-        #try:
-        #    subprocess.run(sqlite_command, shell=True, check=True)
-        #    print(f"✅ Imported {csv_file}")
-        #except subprocess.CalledProcessError as e:
-        #    print(f"❌ Error importing {csv_file}: {e}")
+        df.to_sql(table_name, con=engine, if_exists="append", index=False)
 
     print("🎉 All CSV files have been imported.")
 
@@ -50,4 +45,5 @@ if __name__ == "__main__":
     schema_sql_file = sys.argv[3] if len(sys.argv) == 4 else None
 
     import_csv_to_sqlite(csv_directory, sqlite_db_file, schema_sql_file)
+
 
